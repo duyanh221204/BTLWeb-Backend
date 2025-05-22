788dc33d053982744a2a9db52ae4c4e81d4549d7
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2025-05-22 20:48:17 +0700

    Update database.py

--- configs/database.py
+++ configs/database.py
@@ -1,3 +1,6 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
